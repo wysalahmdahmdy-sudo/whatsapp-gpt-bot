f9a353f1eb0c6085918cc7357fbf3bbc5f2d46fa
wysalahmdahmdy-sudo <wysalahmdahmdy@gmail.com>
2026-05-04 22:05:30 +0430

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
     headless: true,
-    executablePath: '/usr/bin/chromium-browser',
+    channel: 'chromium',
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
