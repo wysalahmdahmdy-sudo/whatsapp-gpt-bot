6097fcea0601dc5556e07e11911eac72c02e93ec
wysalahmdahmdy-sudo <wysalahmdahmdy@gmail.com>
2026-05-05 22:03:07 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
-const { Client } = require('whatsapp-web.js');
+const { Client, LocalAuth } = require('whatsapp-web.js');
+
+console.log('>>> بوټ شروع شو...');
 
 const client = new Client({
-    puppeteer: { headless: true, args: ['--no-sandbox'] }
+    authStrategy: new LocalAuth(),
+    puppeteer: { 
+        headless: true, 
+        args: ['--no-sandbox', '--disable-setuid-sandbox'] 
+    }
 });
 
-client.on('ready', () => console.log('بوټ چالان دی'));
+client.on('qr', (qr) => {
+    console.log('>>> QR کوډ جوړ شو. که 8 رقمي کوډ کار ونکړي دا Scan کړه');
+});
 
-client.on('message', msg => {
-    console.log('مسیج:', msg.body);
-    
-    if (msg.body === 'سلام') msg.reply('وعلیکم السلام +93706989006');
-    else if (msg.body === 'ټوکه') msg.reply('29 ساعته وروسته بوټ جوړ شو 😂');
-    else msg.reply('سلام ولیکه وروره');
+client.on('ready', () => {
+    console.log('✅✅ بوټ وصل شو +93706989006 ✅✅✅');
+});
+
+client.on('auth_failure', (msg) => {
+    console.error('>>> AUTH FAILURE:', msg);
+});
+
+client.on('message', async (msg) => {
+    console.log('>>> نوی مسیج:', msg.body);
+    if (msg.body === 'سلام') await msg.reply('وعلیکم السلام وروره 💚 +93706989006');
+    else if (msg.body === 'ټوکه') await msg.reply('29 ساعته وروسته جوړ شوم 😂');
+    else await msg.reply('سلام ولیکه');
 });
 
 client.initialize();
+
+console.log('>>> د 8 رقمي کوډ غوښتلو لپاره 5 ثانیې انتظار...');
+
+setTimeout(async () => {
+    try {
+        console.log('>>> اوس کوډ غواړم...');
+        const code = await client.requestPairingCode("93706989006");
+        console.log('================================');
+        console.log('>>> ستاسو 8 رقمي کوډ +93706989006:', code);
+        console.log('================================');
+    } catch (err) {
+        console.log('>>> ERROR په کوډ کې:', err.message);
+        console.log('>>> QR به ښکاره شي. هغه Scan کړه');
+    }
+}, 5000);
