2982ea6974aa1a98e78a22384d38fab0da9ba289
wysalahmdahmdy-sudo <wysalahmdahmdy@gmail.com>
2026-05-05 21:57:30 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,17 @@
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const { Client } = require('whatsapp-web.js');
 
 const client = new Client({
-    authStrategy: new LocalAuth(),
     puppeteer: { headless: true, args: ['--no-sandbox'] }
 });
 
-client.on('ready', () => console.log('✅ بوټ چالان شو +93706989006'));
+client.on('ready', () => console.log('بوټ چالان دی'));
 
-client.on('message_create', async msg => {
-    if (msg.fromMe || msg.isGroupMsg) return;
+client.on('message', msg => {
+    console.log('مسیج:', msg.body);
     
-    const text = msg.body;
-    
-    if (text.includes('سلام')) {
-        msg.reply('وعلیکم السلام وروره 💚\n+93706989006');
-    }
-    else if (text.includes('ټوکه')) {
-        msg.reply('ولې بوټ نه ستړی کیږي؟ ځکه په سرور دی 😂🤖');
-    }
-    else if (text.includes('پښتو')) {
-        msg.reply('هو پښتو پوهېږم وروره 🇦🇫');
-    }
-    else if (text.includes('شمیره')) {
-        msg.reply('زما نمبر: +93706989006 📱');
-    }
-    else {
-        msg.reply('`سلام` یا `ټوکه` ولیکه 🤖');
-    }
+    if (msg.body === 'سلام') msg.reply('وعلیکم السلام +93706989006');
+    else if (msg.body === 'ټوکه') msg.reply('29 ساعته وروسته بوټ جوړ شو 😂');
+    else msg.reply('سلام ولیکه وروره');
 });
 
 client.initialize();
-
-setTimeout(async () => {
-    const code = await client.requestPairingCode("93706989006");
-    console.log('>>> کوډ:', code);
-}, 3000);
