aeb72da64e1306d5cf7178aca726817d360358c6
wysalahmdahmdy-sudo <wysalahmdahmdy@gmail.com>
2026-05-05 22:21:52 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 const { default: makeWASocket, useMultiFileAuthState } = require('@whiskeysockets/baileys')
-const pino = require('pino')
 
 async function start() {
-    console.log('>>> شروع +93706989006')
     const { state, saveCreds } = await useMultiFileAuthState('auth')
-    const sock = makeWASocket({
-        auth: state,
-        logger: pino({ level: 'silent' }),
-        printQRInTerminal: false
-    })
+    const sock = makeWASocket({ auth: state, printQRInTerminal: true })
 
-    sock.ev.on('connection.update', async (update) => {
-        if(update.connection === 'open') {
-            console.log('✅ وصل شو +93706989006')
-        }
-        if(!sock.authState.creds.registered) {
+    sock.ev.on('connection.update', async (u) => {
+        if (u.connection === 'open') console.log('✅ وصل شو +93706989006')
+        if (!sock.authState.creds.registered) {
             const code = await sock.requestPairingCode("93706989006")
             console.log('>>> 8 رقمي کوډ:', code)
         }
@@ -24,15 +16,11 @@ async function start() {
 
     sock.ev.on('messages.upsert', async (m) => {
         const msg = m.messages[0]
-        if(!msg.message || msg.key.fromMe) return
+        if (!msg.message || msg.key.fromMe) return
         const text = msg.message.conversation || ''
-
-        if(text.includes('سلام')) {
-            await sock.sendMessage(msg.key.remoteJid, { text: 'وعلیکم السلام +93706989006' })
-        } else {
-            await sock.sendMessage(msg.key.remoteJid, { text: 'سلام ولیکه وروره' })
+        if (text.includes('سلام')) {
+            await sock.sendMessage(msg.key.remoteJid, { text: 'وعلیکم السلام +93706989006 💚' })
         }
     })
 }
-
 start()
